2150b698106303e5db6bf412cf4e04db6dee8e85
Sunil Kumar Kashyap <138206199+sitapuruniversal@users.noreply.github.com>
2023-08-07 22:58:23 +0000

    Accept this change

---
+++ JavaScript Part-2/arrow five.js
@@ -0,0 +1,5 @@
+//Welcome to the arrow fucntion 
+// One parameter and single return statement 
+const square = x => x * x
+// Multiple parameter and sing experessin 
+const sumOfTwoNumber = (x,y) => x + y
---
+++ JavaScript Part-2/four.js
@@ -0,0 +1,58 @@
+// // what is array 
+// // An arrat is data structure that stores an ordered list of Element. 
+// const language = ["Java", "Javascript", "Python", "C++", "Mongo DB"]
+// const len = language.length
+// console.log("Array Lenght = ", len);
+
+// let player = ["Virat","Sachin","Pandaya"]
+// let number = [1,2,3,6,4,7,8,5,9,4,5,8,1]
+
+// let array = ["Hello",20 ,true]
+
+// // Declaration of array 
+// // 1 Using square bracket 
+// let numbers = [1,2,3,6,4,7,8,5,9,4,5,8,1]
+// // 2 Using Array constructor :
+// let number1 = (1,2,3,4,5,6,7,8,9)
+// // 3 Using an array literal 
+// let color = Array( "Red","Green","Blue")
+
+
+// // Array Index and sorting 
+
+// let play = [   ] // this is empty array
+
+// // fruits[0]= "Virat"  // this index is 0
+// // fruits[1]= "Sanya"  // this index is 1
+
+// let colors = Array( "Red","Green","Blue")
+// colors[1]; return
+
+// change Value in array 
+let  res = ["Learning","Watch ","Movig","PW Skill"]
+res[2] = "Building this Project"
+console.log(res);
+
+// Delete keyword in array and its problem 
+const num = [1,2,3,5,8,6]
+console.log("Array Lenght is = ",num.length);
+delete[2];
+console.log("Array Lenght is = ",num.length);
+console.log(num[2]);
+
+// Iterating of array items 
+
+const array = [1,2,3,4]
+
+for (let index = 0; index<array.length; index ++){
+    const element = array[index];
+    console.log(element);
+}
+console.log("Welcome");
+// for of Loop 
+
+const array5 = [1,2,3,4,5,6,7]
+array5.forEach(element => {
+    console.log(NodeIterator);
+    
+});
